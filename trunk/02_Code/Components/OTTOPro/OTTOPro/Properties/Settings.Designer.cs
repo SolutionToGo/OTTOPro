d559f06c04b00c69347595e647cafdba75d68436
Mahi1008 <mahendra.khanannavar@solutiontogo.de>
2017-04-05 12:33:11 +0530

    setting changes

--- trunk/02_Code/Components/OTTOPro/OTTOPro/Properties/Settings.Designer.cs
+++ trunk/02_Code/Components/OTTOPro/OTTOPro/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OTTOPro.Properties
-{
-
-
+namespace OTTOPro.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.1.50\\SQLEXPRESS;Initial Catalog=OTTOPro_Dev;User ID=stguser;P" +
+            "assword=stg@#987")]
+        public string OTTOPro {
+            get {
+                return ((string)(this["OTTOPro"]));
             }
         }
     }
