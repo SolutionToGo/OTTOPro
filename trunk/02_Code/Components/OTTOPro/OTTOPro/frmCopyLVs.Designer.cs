7de70f5b5da008b251fb5667cab993958fcfca44
mahi1008 <mahendra.khanannavar@solutiontogo.de>
2017-05-30 12:24:40 +0530

    copy Lv Changes
    Signed-off-by: mahi1008 <mahendra.khanannavar@solutiontogo.de>

--- trunk/02_Code/Components/OTTOPro/OTTOPro/frmCopyLVs.Designer.cs
+++ trunk/02_Code/Components/OTTOPro/OTTOPro/frmCopyLVs.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
             this.btnOk = new DevExpress.XtraEditors.SimpleButton();
             this.radioGroupType = new DevExpress.XtraEditors.RadioGroup();
             this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
@@ -61,8 +61,20 @@
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
+            // btnCancel
+            // 
+            this.btnCancel.Image = global::OTTOPro.Properties.Resources.Cancel_16x16;
+            this.btnCancel.Location = new System.Drawing.Point(222, 131);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(79, 22);
+            this.btnCancel.StyleController = this.layoutControl1;
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
             // btnOk
             // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnOk.Image = global::OTTOPro.Properties.Resources.Apply_16x16;
             this.btnOk.Location = new System.Drawing.Point(142, 131);
             this.btnOk.Name = "btnOk";
@@ -70,11 +82,13 @@
             this.btnOk.StyleController = this.layoutControl1;
             this.btnOk.TabIndex = 5;
             this.btnOk.Text = "OK";
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // radioGroupType
             // 
             this.radioGroupType.Location = new System.Drawing.Point(12, 12);
             this.radioGroupType.Name = "radioGroupType";
+            this.radioGroupType.Properties.AllowMouseWheel = false;
             this.radioGroupType.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioGroupType.Properties.Appearance.Options.UseFont = true;
             this.radioGroupType.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
@@ -128,16 +142,6 @@
             this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem2.TextVisible = false;
             // 
-            // btnCancel
-            // 
-            this.btnCancel.Image = global::OTTOPro.Properties.Resources.Cancel_16x16;
-            this.btnCancel.Location = new System.Drawing.Point(222, 131);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(79, 22);
-            this.btnCancel.StyleController = this.layoutControl1;
-            this.btnCancel.TabIndex = 6;
-            this.btnCancel.Text = "Cancel";
-            // 
             // layoutControlItem3
             // 
             this.layoutControlItem3.Control = this.btnCancel;
@@ -162,6 +166,7 @@
             this.Name = "frmCopyLVs";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Copy To";
+            this.Load += new System.EventHandler(this.frmCopyLVs_Load);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.radioGroupType.Properties)).EndInit();
--- trunk/02_Code/Components/OTTOPro/OTTOPro/frmCopyLVs.cs
+++ trunk/02_Code/Components/OTTOPro/OTTOPro/frmCopyLVs.cs
@@ -13,9 +13,30 @@ namespace OTTOPro
 {
     public partial class frmCopyLVs : DevExpress.XtraEditors.XtraForm
     {
+        private int _type; 
         public frmCopyLVs()
         {
             InitializeComponent();
+        }
+
+        public int type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmCopyLVs_Load(object sender, EventArgs e)
+        {
+            radioGroupType.SelectedIndex = 1;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            _type = radioGroupType.SelectedIndex;
         }
     }
 }
